756a1db12b23a9b06adb9ea6054353fd3dfd80db
nkleinmann <nekleinmann@gmail.com>
2020-09-08 21:03:58 -0400

    initial prompt

--- employee.js
+++ employee.js
@@ -2,14 +2,13 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "AustraliA2!",
   database: "employeeDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
 });
---
+++ inquirer.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+
+startPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'whatWouldYouLikeToDo',
+            message: 'What would you like to do??',
+            choices: [
+                'View All Employees',
+                'View All Employees by Department',
+                'View All Employees By Manager',
+                'Add Employee',
+                'Add Department',
+                'Add Role',
+                'Update Employee Role'
+            ]
+        }
+    ])
+}
+
+startPrompt();
