a96d6be40ef062840ca27b78b3ec90158b361aa4
nkleinmann <nekleinmann@gmail.com>
2020-09-12 15:27:47 -0400

    First 2 cases in switch statement

--- inquirer.js
+++ inquirer.js
@@ -9,7 +9,7 @@ startPrompt = (connection) => {
             choices: [
                 'View All Employees',
                 'View All Employees by Department',
-                'View All Employees By Manager',
+                'View All Employees By Role',
                 'Add Employee',
                 'Add Department',
                 'Add Role',
@@ -19,6 +19,42 @@ startPrompt = (connection) => {
     ]).then(function (answers) {
         switch (answers.whatWouldYouLikeToDo) {
             case "View All Employees":
+                connection.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;', function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                });
+                break;
+            case "View All Employees by Department":
+                connection.query('SELECT employee.id, employee.first_name, employee.last_name, department.name AS department FROM employee LEFT JOIN department on employee.role_id = department.id;', function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                });
+                break;
+            case "View All Employees by Role":
+                connection.query('SELECT * from employee', function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                });
+                break;
+            case "Add Employee":
+                connection.query('SELECT * from employee', function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                });
+                break;
+            case "Add Department":
+                connection.query('SELECT * from employee', function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                });
+                break;
+            case "Add Role":
+                connection.query('SELECT * from employee', function (err, data) {
+                    if (err) throw err;
+                    console.table(data);
+                });
+                break;
+            case "Update Employee Role":
                 connection.query('SELECT * from employee', function (err, data) {
                     if (err) throw err;
                     console.table(data);
